5ea77099c29c616613d0d3072402cd6cbdd9092c
manos5545 <manos5545@yahoo.gr>
2018-05-15 22:18:20 +0300

    Added Threadpool and changed Innsort
    Threads are created and next step to assign job and finish them.

--- quicksort.c
+++ quicksort.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <pthread.h>
 
 #define MESSAGES 20
+#define numOfThreads 8
+#define N 10000
+#define CUTOFF 1000
+
+
 
 struct message{
-	int id;
-	char minima[];
+	int startOfArray;
+	int finishOfArray;
 };
 typedef struct message message;
 
@@ -17,6 +21,12 @@ struct jobQueue{
 	int finish;
 };
 typedef struct jobQueue jobQueue;
+
+void QueueInit(jobQueue *q)
+{
+	q->start = -1;
+	q->finish = -1;
+}
 
 int QueueEmpty(jobQueue q)
 {
@@ -28,15 +38,77 @@ int QueueFull(jobQueue q)
 	return q.start==(q.finish)%MESSAGES;
 }
 
+void inssort(double *a,int n) {
+int i,j;
+double t;
+  
+  for (i=1;i<n;i++) {
+    j = i;
+    while ((j>0) && (a[j-1]>a[j])) {
+      t = a[j-1];  a[j-1] = a[j];  a[j] = t;
+      j--;
+    }
+  }
+
+}
+
+
+void quicksort(double *a,int n) {
+	
+int first,last,middle;
+double t,p;
+int i,j;
+
+  // check if below cutoff limit
+  if (n<=CUTOFF) {
+    inssort(a,n);
+    message oloklirosi;
+    oloklirosi.startOfArray = 0;
+    oloklirosi.finishOfArray = n-1;
+    printf("Sort completed on partition %d and %d", oloklirosi.startOfArray, oloklirosi.finishOfArray);
+    return;
+  }
+  
+  // take first, last and middle positions
+  first = 0;
+  middle = n/2;
+  last = n-1;  
+  
+  // put median-of-3 in the middle
+  if (a[middle]<a[first]) { t = a[middle]; a[middle] = a[first]; a[first] = t; }
+  if (a[last]<a[middle]) { t = a[last]; a[last] = a[middle]; a[middle] = t; }
+  if (a[middle]<a[first]) { t = a[middle]; a[middle] = a[first]; a[first] = t; }
+    
+  // partition (first and last are already in correct half)
+  p = a[middle]; // pivot
+  for (i=1,j=n-2;;i++,j--) {
+    while (a[i]<p) i++;
+    while (p<a[j]) j--;
+    if (i>=j) break;
+
+    t = a[i]; a[i] = a[j]; a[j] = t;      
+  }
+   
+  // recursively sort halves
+  quicksort(a,i);
+  quicksort(a+i,n-i);
+  
+}
+
+void *ThreadJob(void *args){
+	quicksort(args, N);
+}
 // global integer buffer
 message globalBuffer[MESSAGES];
-
+// global avail messages count (0 or 1)
 int global_availmsg = 0;	// empty
 
-
+// condition variable, signals a put operation (receiver waits on this)
 pthread_cond_t msg_in = PTHREAD_COND_INITIALIZER;
+// condition variable, signals a get operation (sender waits on this)
 pthread_cond_t msg_out = PTHREAD_COND_INITIALIZER;
 
+// mutex protecting common resources
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 
@@ -56,8 +128,8 @@ void *producer_thread(void *args) {
       					   // When we wake up, mutex will be locked by us again. 
     }
     // send message
-    globalBuffer[i].id = i;
-    printf("Producer: sending msg %d\n",globalBuffer[i].id);
+    globalBuffer[i].startOfArray = i;
+    printf("Producer: sending msg %d\n",globalBuffer[i].startOfArray);
     global_availmsg = 1;
     
     // signal the receiver that something was put in buffer
@@ -66,6 +138,8 @@ void *producer_thread(void *args) {
     // unlock mutex
     pthread_mutex_unlock(&mutex);
   }
+  
+  // exit and let be joined
   pthread_exit(NULL); 
 }
   
@@ -91,11 +165,37 @@ void *consumer_thread(void *args) {
     // unlock mutex
     pthread_mutex_unlock(&mutex);
   }
+  
+  // exit and let be joined
   pthread_exit(NULL); 
 }
 
-
 int main() {
+  double *a;
+  int i;
+  
+  //mallocing array
+  a = (double *)malloc(N*sizeof(double));
+  if (a==NULL) {
+    printf("error in malloc\n");
+    exit(1);
+  }
+  message firstMessage;
+  firstMessage.startOfArray = 0;
+  firstMessage.finishOfArray = N;
+
+  // fill array with random numbers
+  srand(time(NULL));
+  for (i=0;i<N;i++) {
+    a[i] = (double)rand()/RAND_MAX;
+  }
+  
+  //Creating the threadpool
+  pthread_t threads[numOfThreads];
+  int t;
+  for (t=0; t<numOfThreads; t++){
+    pthread_create(&(threads[t]), NULL, NULL, NULL);
+  }
   
   pthread_t producer,consumer;
   
